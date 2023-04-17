3d562dac72df7d1951dc76ca1e2ff278d6e06c26
27tr <114559221+MCDream3@users.noreply.github.com>
2023-04-17 13:17:24 +0000

    dodano kartkówkę z dnia 17.04

---
+++ kartkowka_17.04.py
@@ -0,0 +1,69 @@
+#Zad.1 gr 1
+
+t = input()
+
+for i in range(len(t)):
+  print(t[0:i+1])
+
+#Zad.2
+
+d = input()
+ilosc = 0
+for i in range(len(d)):
+  if d[i] == d[i+1]:
+    ilosc += 1
+print(ilosc)
+
+
+
+
+#Zad.1 gr 2
+
+e = input()
+L = set(e)
+print(len(L))
+
+#Zad.2
+
+f = input()
+wyjscie = " "
+for i in range(len(f)+ f[-i-1]):
+  wyjscie += f[i] + f[-i-1]
+if len(s) % 2:
+  wyjscie += f[len(s)//2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+i didn't do anything on this classwork...it's just correct answer
+the fuck is wrong with me??
+i can't do anything...
+im useless..
+im worthless...
+some people were right.. maby programming is not for me...
+if someone read it... congrats you know now a lot...
+maby the only way is to suicide? just because I'm not going to feel the pain anymore..
+ain't sure if it's the correct answer...
+😓
+if anybody read this... please don't fuck me off...
+neverending pain is controlling me...
+if my best friend read this... I'm sorry bro...just please don't leave me k?
